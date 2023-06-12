db486611ac465475480bae9db7b9de021df3da91
Purnomo-Ribut <84133326+Purnomo-Ribut@users.noreply.github.com>
2023-06-12 03:01:07 -0700

    Update index.py

--- index.py
+++ index.py
@@ -24,6 +24,35 @@ import pandas as pd
 #from sklearn.neighbors import KNeighborsClassifier
 #from sklearn.tree import DecisionTreeClassifier
 
+if option == 'Home' or option == '':
+    st.write("""# Halaman Utama""") #menampilkan halaman utama
+elif option == 'Dataframe':
+    st.write("""## Dataframe""") #menampilkan judul halaman dataframe
+
+    #membuat dataframe dengan pandas yang terdiri dari 2 kolom dan 4 baris data
+    df = pd.DataFrame({
+        'Column 1':[1,2,3,4],
+        'Column 2':[10,12,14,16]
+    })
+    df #menampilkan dataframe
+elif option == 'Chart':
+    st.write("""## Draw Charts""") #menampilkan judul halaman 
+
+    #membuat variabel chart data yang berisi data dari dataframe
+    #data berupa angka acak yang di-generate menggunakan numpy
+    #data terdiri dari 2 kolom dan 20 baris
+    chart_data = pd.DataFrame(
+        np.random.randn(20,2), 
+        columns=['a','b']
+    )
+    #menampilkan data dalam bentuk chart
+    st.line_chart(chart_data)
+    #data dalam bentuk tabel
+    chart_data
+
+
+
+
 st.title('E-LEARNING STUDENTS REACTIONS')
 st.write ("""
        Purnomo Ribut | 200411100156 <br>

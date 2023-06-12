456114d08ef610781a500d9dea9772355d99bf50
Purnomo-Ribut <84133326+Purnomo-Ribut@users.noreply.github.com>
2023-06-12 05:58:37 -0700

    Update index.py

--- index.py
+++ index.py
@@ -150,6 +150,127 @@ elif option == 'Modeling':
 	data["penjelasan"]=data["penjelasan"].apply(remove_punctuation)
 	st.write(""" Hasil Hapus Tanda Hubung """)
 	data
+	
+	from string import punctuation
+	import nltk 
+	nltk.download("punkt")
+
+	from nltk.tokenize import word_tokenize
+	data["hasil"]=data["penjelasan"].apply(lambda x: nltk.word_tokenize(x))
+	data["hasil"]
+
+	data
+
+	normalize = pd.read_excel("/content/drive/MyDrive/parawisata /Normalization Data.xlsx")
+	normalize_word_dict={}
+
+	for row in normalize.iterrows():
+	  if row[0] not in normalize_word_dict:
+	    normalize_word_dict[row[0]] = row[1]
+
+	def normalized_term(comment):
+	  return [normalize_word_dict[term] if term in normalize_word_dict else term for term in comment]
+
+	data['comment_normalize'] = data['hasil'].apply(normalized_term)
+	data['comment_normalize'].head(10)
+
+	#stopword removal
+	nltk.download("stopwords")
+	from nltk.corpus import stopwords
+	txt_stopwords = stopwords.words("indonesian")
+
+	def stopword_removal(filter):
+	  filter = [word for word in filter if word not in txt_stopwords]
+	  return filter
+	data["stopwords_removal"]=data["comment_normalize"].apply(stopword_removal)
+	data["stopwords_removal"]
+
+	#removal2
+	data_stopwords=pd.read_excel("/content/drive/MyDrive/parawisata /list_stopwords.xlsx")
+
+	def stopword_removal2 (filter):
+	  filter =[word for word in filter if word not in data_stopwords]
+	  return filter
+	data["stopwords_removal_final"]=data["stopwords_removal"].apply(stopword_removal2)
+	data["stopwords_removal_final"]
+
+	"""## Stemming"""
+
+	#proses stem
+	from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+	import string
+	import swifter
+	factory = StemmerFactory()
+	stemmer = factory.create_stemmer()
+
+	def stemming (term):
+	  return stemmer.stem(term)
+
+	term_dict = {}
+	for document in data['stopwords_removal_final']:
+	  for term in document:
+	    if term not in term_dict:
+	      term_dict[term] = ''
+	print(len(term_dict))
+
+	for term in term_dict:
+	  term_dict[term] = stemming(term)
+	  print(term,":",term_dict[term])
+
+	print(term_dict)
+
+	def get_stemming(document):
+	  return [term_dict[term] for term in document]
+
+	data['stemming'] = data['stopwords_removal_final'].swifter.apply(get_stemming)
+
+	print(data['stemming'])
+	data.head(20)
+
+	#Perhitungan TF-IDF
+	def joinkata(data):
+	  kalimat = ""
+	  for i in data:
+	    kalimat += i
+	    kalimat += " "
+	  return kalimat
+
+	text = data['stemming'].swifter.apply(joinkata)
+	text
+
+	from sklearn.feature_extraction.text import TfidfVectorizer
+	tfidf_vectorizer = TfidfVectorizer()
+	tfidf_separate = tfidf_vectorizer.fit_transform(text)
+
+	df_tfidf = pd.DataFrame(
+	    tfidf_separate.toarray(), columns=tfidf_vectorizer.get_feature_names_out(), index=data.index
+	)
+	X = df_tfidf.values
+	Y = data['label']
+	df_tfidf
+
+	Y
+
+	from sklearn.model_selection import train_test_split
+	X_train, X_test, Y_train, Y_test = train_test_split( 
+	    X, Y, test_size = 0.3, random_state = 100)
+	print("Jumlah Data training : ", len(X_train))
+	print("Jumlah Data test : ", len(X_test))
+
+	from sklearn.naive_bayes import GaussianNB
+	gnb_model = GaussianNB()
+	gnb_model.fit(X_train, Y_train)
+
+	from wordcloud import WordCloud
+
+	allWords = ' '.join([twts for twts in data['penjelasan']])
+	wordCloud = WordCloud(width=1600, height=800, random_state=30, max_font_size=200, min_font_size=20).generate(allWords)
+
+	from sklearn.metrics import accuracy_score
+	Y_pred = gnb_model.predict(X_test)
+	print (" GNB Accuracy : ",
+	    accuracy_score(Y_test,Y_pred)*100)
+
     
 	    
 
@@ -201,125 +322,3 @@ data.shape
 
 
 
-from string import punctuation
-
-
-
-import nltk 
-nltk.download("punkt")
-
-from nltk.tokenize import word_tokenize
-data["hasil"]=data["penjelasan"].apply(lambda x: nltk.word_tokenize(x))
-data["hasil"]
-
-data
-
-normalize = pd.read_excel("/content/drive/MyDrive/parawisata /Normalization Data.xlsx")
-normalize_word_dict={}
-
-for row in normalize.iterrows():
-  if row[0] not in normalize_word_dict:
-    normalize_word_dict[row[0]] = row[1]
-
-def normalized_term(comment):
-  return [normalize_word_dict[term] if term in normalize_word_dict else term for term in comment]
-
-data['comment_normalize'] = data['hasil'].apply(normalized_term)
-data['comment_normalize'].head(10)
-
-#stopword removal
-nltk.download("stopwords")
-from nltk.corpus import stopwords
-txt_stopwords = stopwords.words("indonesian")
-
-def stopword_removal(filter):
-  filter = [word for word in filter if word not in txt_stopwords]
-  return filter
-data["stopwords_removal"]=data["comment_normalize"].apply(stopword_removal)
-data["stopwords_removal"]
-
-#removal2
-data_stopwords=pd.read_excel("/content/drive/MyDrive/parawisata /list_stopwords.xlsx")
-
-def stopword_removal2 (filter):
-  filter =[word for word in filter if word not in data_stopwords]
-  return filter
-data["stopwords_removal_final"]=data["stopwords_removal"].apply(stopword_removal2)
-data["stopwords_removal_final"]
-
-"""## Stemming"""
-
-#proses stem
-from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
-import string
-import swifter
-factory = StemmerFactory()
-stemmer = factory.create_stemmer()
-
-def stemming (term):
-  return stemmer.stem(term)
-
-term_dict = {}
-for document in data['stopwords_removal_final']:
-  for term in document:
-    if term not in term_dict:
-      term_dict[term] = ''
-print(len(term_dict))
-
-for term in term_dict:
-  term_dict[term] = stemming(term)
-  print(term,":",term_dict[term])
-
-print(term_dict)
-
-def get_stemming(document):
-  return [term_dict[term] for term in document]
-
-data['stemming'] = data['stopwords_removal_final'].swifter.apply(get_stemming)
-
-print(data['stemming'])
-data.head(20)
-
-#Perhitungan TF-IDF
-def joinkata(data):
-  kalimat = ""
-  for i in data:
-    kalimat += i
-    kalimat += " "
-  return kalimat
-
-text = data['stemming'].swifter.apply(joinkata)
-text
-
-from sklearn.feature_extraction.text import TfidfVectorizer
-tfidf_vectorizer = TfidfVectorizer()
-tfidf_separate = tfidf_vectorizer.fit_transform(text)
-
-df_tfidf = pd.DataFrame(
-    tfidf_separate.toarray(), columns=tfidf_vectorizer.get_feature_names_out(), index=data.index
-)
-X = df_tfidf.values
-Y = data['label']
-df_tfidf
-
-Y
-
-from sklearn.model_selection import train_test_split
-X_train, X_test, Y_train, Y_test = train_test_split( 
-    X, Y, test_size = 0.3, random_state = 100)
-print("Jumlah Data training : ", len(X_train))
-print("Jumlah Data test : ", len(X_test))
-
-from sklearn.naive_bayes import GaussianNB
-gnb_model = GaussianNB()
-gnb_model.fit(X_train, Y_train)
-
-from wordcloud import WordCloud
-
-allWords = ' '.join([twts for twts in data['penjelasan']])
-wordCloud = WordCloud(width=1600, height=800, random_state=30, max_font_size=200, min_font_size=20).generate(allWords)
-
-from sklearn.metrics import accuracy_score
-Y_pred = gnb_model.predict(X_test)
-print (" GNB Accuracy : ",
-    accuracy_score(Y_test,Y_pred)*100)
